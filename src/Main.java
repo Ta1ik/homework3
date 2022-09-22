61f100311f77537b5e43e5068db603580aec5be4
Hw2 <Kucherenko.vv@mail.ru>
2022-09-22 12:05:33 +0800

    Hw3 commit1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 2147483647;
+        byte b = 127;
+        short s = 32767;
+        long l = 9_223_372_036_854_775_807L;
+        float f = 100.1234567F;
+        double d = 1000.123456789;
+        System.out.println("Значение переменой i с типом int =" + i);
+        System.out.println("Значение переменой b с типом byte =" + b);
+        System.out.println("Значение переменой s с типом short =" + s);
+        System.out.println("Значение переменой l с типом long =" + l);
+        System.out.println("Значение переменой f с типом float =" + f);
+        System.out.println("Значение переменой d с типом double =" + d);
+
+    }
+}
